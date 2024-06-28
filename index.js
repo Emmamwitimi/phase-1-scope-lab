ceccbd56af0d6bdb1531347ffbebd630127a9c12
Emmaculate <emmamwitimi14@gmail.com>
2024-06-28 07:11:49 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+function getCustomerName() {
+    return customerName;
+  }
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  } 
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+  }
+  const leastFavoriteCustomer = 'someone';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another person';
+  } 
